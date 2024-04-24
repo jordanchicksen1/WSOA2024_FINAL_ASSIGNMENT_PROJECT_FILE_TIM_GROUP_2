9887dc770520ca60263069f9b5e76895cc7ceff5
RamzCas <2749414@students.wits.ac.za>
2024-04-24 09:52:04 +0200

    done nothing

--- Assets/scripts/Ray.cs
+++ Assets/scripts/Ray.cs
@@ -4,24 +4,24 @@ using UnityEngine;
 
 public class UniversalCheck : MonoBehaviour
 {
-   Ray rayz;
+   Ray rayDown;
    RaycastHit[] hit;
 
    public GameObject Univeral;
-    public GameObject P1;
+   public GameObject P1;
    public GameObject P2;
-    public Vector3 direction = new Vector3(0,0, 0); 
+   public Vector3 direction = new Vector3(0,0, 0); 
 
     public void Start()
     {
-        rayz = new Ray(Univeral.transform.position, direction);
-       
+        rayDown = new Ray(Univeral.transform.position, direction);
     }
 
-   public void CheckHit() 
-   {
-        hit = Physics.RaycastAll(rayz);
-        if (hit.Length > 0) 
+    public void CheckHit()
+    {
+        hit = Physics.RaycastAll(rayDown);
+
+        if (P1 != null) 
         {
             Debug.Log("hit");
         }
