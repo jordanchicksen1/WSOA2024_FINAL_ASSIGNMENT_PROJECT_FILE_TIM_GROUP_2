3feef5e3f8d0874935bac2f40e9a148ba86b2946
RamzCas <2749414@students.wits.ac.za>
2024-04-24 10:56:31 +0200

    tried to make rotating boards
    did not work

---
+++ Assets/scripts/BoardTurn.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardTurn : MonoBehaviour
+{
+    //public GameObject board1;
+    //public GameObject board2;
+    //public GameObject board3;
+    //public GameObject board4;
+
+    //public float spin = 90f;
+
+    //public void B1Left() 
+    //{
+
+    //    board1.transform.Rotate(0, 0, spin);
+     
+    //    //transform.RotateAround(board1.transform.position,Vector3.right,spin * Time.deltaTime);
+    //    Debug.Log("roate");
+ }
+
+   
+
