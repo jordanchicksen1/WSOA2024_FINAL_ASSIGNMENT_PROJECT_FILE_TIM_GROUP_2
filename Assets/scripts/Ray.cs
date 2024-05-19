82593be7a8c7694e1b039c32e8baa60ea43a17d0
RamzCas <2749414@students.wits.ac.za>
2024-05-19 17:22:44 +0200

    Raycast
    Made a scene to work on Raycasts
    got it some what working (raycast can detect colliders)

--- Assets/scripts/Ray.cs
+++ Assets/scripts/Ray.cs
@@ -1,29 +1,83 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class UniversalCheck : MonoBehaviour
 {
-   Ray rayDown;
-   RaycastHit[] hit;
 
-   public GameObject Univeral;
-   public GameObject P1;
-   public GameObject P2;
-   public Vector3 direction = new Vector3(0,0, 0); 
+    public SpriteRenderer Renderer;
+    public GameObject circle;
+    /*Ray rayDown;
+    RaycastHit[] hit;
 
-    public void Start()
+    public GameObject Univeral;
+    public GameObject P1;
+    public GameObject P2;
+    public Vector3 direction = new Vector3(0,0, 0); 
+
+     public void Start()
+     {
+         rayDown = new Ray(Univeral.transform.position, direction);
+     }
+
+     public void CheckHit()
+     {
+         hit = Physics.RaycastAll(rayDown);
+
+         if (P1 != null) 
+         {
+             Debug.Log("hit");
+         }
+    }*/
+
+    private void FixedUpdate()
     {
-        rayDown = new Ray(Univeral.transform.position, direction);
-    }
+        /*RaycastHit2D hitDown = Physics2D.Raycast(transform.position, -Vector2.up);
+        RaycastHit2D hitRight= Physics2D.Raycast (transform.position, Vector2.right);
+        RaycastHit2D hitUp= Physics2D.Raycast (transform.position,Vector2.up);    
+        RaycastHit2D hitLeft = Physics2D.Raycast(transform.position, -Vector2.right);
+        
 
-    public void CheckHit()
-    {
-        hit = Physics.RaycastAll(rayDown);
-
-        if (P1 != null) 
+        if(hitDown.collider) 
         {
-            Debug.Log("hit");
+            Debug.Log("hitDown");
+            Renderer.color = Color.blue;
         }
-   }
+
+        if(hitRight.collider) 
+        {
+            Debug.Log("hitRight");
+            Renderer.color = Color.green;
+        }
+       
+        if(hitUp.collider) 
+        {
+            Debug.Log("hitUp");
+            Renderer.color = Color.yellow;
+        }
+
+        if (hitLeft.collider) 
+        {
+            Debug.Log("hitLeft");
+            Renderer.color = Color.cyan;
+        }*/
+
+        RaycastHit2D[] hits;
+        hits = Physics2D.RaycastAll(transform.position, Vector2.right);
+
+        for (int i = 0; i < hits.Length; i++) 
+        {
+        RaycastHit2D hit = hits[i];
+        Renderer rend =hit.transform.GetComponent<Renderer>();
+
+            if (rend) 
+            {
+                Debug.Log("hit");
+                //Renderer.color = Color.blue;
+            }
+
+           
+        }
+    }
 }
