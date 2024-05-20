0f6dc223e65cfcd7194e82430e5c1a8e1d753bea
jordanchicksen1 <2333640@students.wits.ac.za>
2024-05-20 10:19:31 +0200

    fixed merger issues??
    there are stashed changes in gameManager?? i don't know what that means

--- Assets/scripts/gameManager.cs
+++ Assets/scripts/gameManager.cs
@@ -12,17 +12,63 @@ public class gameManager : MonoBehaviour
     public titleScreen titleScreen;
     public AudioSource src;
     public AudioClip sfx1;
+
+   
     //int[,] boardUpState; //0 is empty, 1 is the universal token, 2 is player1 and 3 is player2
     //public int boardUpHeight = 3;
     //public int boardUpWidth = 3;
     //// 0 0 0
     // 0 0 0
     // 0 0 0
+<<<<<<< Updated upstream
     
         
         //boardUpState = new int[boardUpHeight, boardUpWidth];
     
     
+=======
+    void Start()
+    {
+        titleScreen.p1Turn();
+        
+        //boardUpState = new int[boardUpHeight, boardUpWidth];
+    }
+    void Update()
+    {
+      
+       if (Input.GetMouseButtonDown(0) && player1 == true)
+       {
+           Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+           Vector3 offset = new Vector3(0, 0, 10);
+           Instantiate(p1token, pos + offset, Quaternion.identity);
+           Debug.Log("left click");
+            player1 = false;
+            player2 = true;
+            titleScreen.p1TurnFalse();
+            titleScreen.p2Turn();
+            src.clip = sfx1;
+            src.Play();
+
+       
+            
+       }
+
+       if (Input.GetMouseButtonDown(1) && player2 == true)
+       {
+            Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 offset = new Vector3(0, 0, 10);
+            Instantiate(p2token, pos + offset, Quaternion.identity);
+            Debug.Log("right click");
+            player2 = false;
+            player1 = true;
+            titleScreen.p1Turn();
+            titleScreen.p2TurnFalse();
+            src.clip = sfx1;
+            src.Play();
+       }
+
+    }
+>>>>>>> Stashed changes
 
     //void UpdateBoardUpState()
     //{
